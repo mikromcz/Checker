2181773d265c8eba01880dab7b4a21cdb6b7847a
Roman Mikulka <mikrom@mikrom.cz>
2024-02-02 15:04:38 +0100

    v1.3.0

--- Checker.lib.pas
+++ Checker.lib.pas
@@ -4,10 +4,12 @@
   Www: http://geoget.ararat.cz/doku.php/user:skript:checker
   Forum: http://www.geocaching.cz/forum/viewthread.php?forum_id=20&thread_id=25822
   Author: mikrom, http://mikrom.cz
-  Version: 0.1.2.0
+  Version: 0.1.3.0
 
   tohle by mohlo bejt zajimavy: http://www.regular-expressions.info/duplicatelines.html
 }
+
+const debug='0';
 
 {ocisteni url, nekdy se to z listingu vyparsuje blbe, tohle to celkem uspesne resi}
 function TrimUrl(url:String):String;
@@ -44,83 +46,92 @@ begin
   end;
 
   {pro jistotu zda jsou souradnice nenulove}
-  if coord<>'' then begin
+  if coord<>'???' then begin
     {N50°30.123' E015°29.456' rozdelime na jednotlive sekce oddelene mezerami => N 50 30 123 E 015 29 456}
     coord:=RegexReplace('(N|S)(\d+)°(\d+)\.(\d+)''\s(E|W)(\d+)°(\d+)\.(\d+)''',coord,'$1 $2 $3 $4 $5 $6 $7 $8',true);
 
     {zjistit typ overovaci sluzby - geocheck.org, geochecker.com, evince.locusprime.net, atd..}
-    if RegexFind('(https?://|www\.)(geocheck\.org|geotjek\.dk)\/geo_inputchkcoord([^"'']+)',GC.LongDescription) then begin
-      url:=RegExSubstitute('(https?://|www\.)(geocheck\.org|geotjek\.dk)\/geo_inputchkcoord([^"'']+)',GC.LongDescription,'$0#'); // parsnout url z listingu, GC.LongDescription (konci '#')
+    {
+    GEOCHECK
+    url: geocheck.org/geo_inputchkcoord.php?gid=61241961c72ab1d-b813-47da-bf03-07c67bb81ac9
+    captcha: ano
+    }
+    if RegexFind('https?://(www\.)?(geocheck\.org|geotjek\.dk)\/geo_inputchkcoord([^"'']+)',GC.LongDescription) then begin
+      url:=RegExSubstitute('https?://(www\.)?(geocheck\.org|geotjek\.dk)\/geo_inputchkcoord([^"'']+)',GC.LongDescription,'$0#'); // parsnout url z listingu, GC.LongDescription (konci '#')
       service:='geocheck';
-      {
-      url: geocheck.org/geo_inputchkcoord.php?gid=61241961c72ab1d-b813-47da-bf03-07c67bb81ac9
-      captcha: ano
-      }
     end
-    else if RegexFind('(https?://|www\.)geochecker\.com\/index\.php([^"'']+)',GC.LongDescription) then begin
-      url:=RegExSubstitute('(https?://|www\.)geochecker\.com\/index\.php([^"'']+)',GC.LongDescription,'$0#'); // parsnout url z listingu, GC.LongDescription (konci '#')
+    {
+    GEOCHECKER
+    url: http://www.geochecker.com/index.php?code=e380cf72d82fa02a81bf71505e8c535c&action=check&wp=4743324457584d&name=536b6c656e696b202d20477265656e20486f757365
+    captcha: ne
+    }
+    else if RegexFind('https?://(www\.)?geochecker\.com\/index\.php([^"'']+)',GC.LongDescription) then begin
+      url:=RegExSubstitute('https?://(www\.)?geochecker\.com\/index\.php([^"'']+)',GC.LongDescription,'$0#'); // parsnout url z listingu, GC.LongDescription (konci '#')
       service:='geochecker';
-      {
-      url: http://www.geochecker.com/index.php?code=e380cf72d82fa02a81bf71505e8c535c&action=check&wp=4743324457584d&name=536b6c656e696b202d20477265656e20486f757365
-      captcha: ne
-      }
     end
-    else if RegexFind('(https?://|www\.)evince\.locusprime\.net\/cgi-bin\/([^"'']+)',GC.LongDescription) then begin
-      url:=RegExSubstitute('(https?://|www\.)evince\.locusprime\.net\/cgi-bin\/([^"'']+)',GC.LongDescription,'$0#'); // parsnout url z listingu, GC.LongDescription (konci '#')
+    {
+    EVINCE
+    url: http://evince.locusprime.net/cgi-bin/index.cgi?q=d0ZNzQeHKReGKzr
+    captcha: ano
+    }
+    else if RegexFind('https?://(www\.)?evince\.locusprime\.net\/cgi-bin\/([^"'']+)',GC.LongDescription) then begin
+      url:=RegExSubstitute('https?://(www\.)?evince\.locusprime\.net\/cgi-bin\/([^"'']+)',GC.LongDescription,'$0#'); // parsnout url z listingu, GC.LongDescription (konci '#')
       service:='evince';
-      {
-      url: http://evince.locusprime.net/cgi-bin/index.cgi?q=d0ZNzQeHKReGKzr
-      captcha: ano
-      }
     end
-    else if RegexFind('(https?://|www\.)(geo\.hermansky\.net|speedygt\.ic\.cz\/gps)\/index\.php\?co\=checker([^"'']+)',GC.LongDescription) then begin
-      url:=RegExSubstitute('(https?://|www\.)(geo\.hermansky\.net|speedygt\.ic\.cz\/gps)\/index\.php\?co\=checker([^"'']+)',GC.LongDescription,'$0#'); // parsnout url z listingu, GC.LongDescription (konci '#')
+    {
+    HERMANSKY
+    url: http://geo.hermansky.net/index.php?co=checker&code=2542e4245f80d4f7783e41ed7503fba6b3c8cc3188ff05
+    captcha: ne
+    }
+    else if RegexFind('https?://(www\.)?(geo\.hermansky\.net|speedygt\.ic\.cz\/gps)\/index\.php\?co\=checker([^"'']+)',GC.LongDescription) then begin
+      url:=RegExSubstitute('https?://(www\.)?(geo\.hermansky\.net|speedygt\.ic\.cz\/gps)\/index\.php\?co\=checker([^"'']+)',GC.LongDescription,'$0#'); // parsnout url z listingu, GC.LongDescription (konci '#')
       service:='hermansky';
-      {
-      url: http://geo.hermansky.net/index.php?co=checker&code=2542e4245f80d4f7783e41ed7503fba6b3c8cc3188ff05
-      captcha: ne
-      }
     end
-    else if RegexFind('(https?://|www\.)geo\.komurka\.cz\/check\.php([^"'']+)',GC.LongDescription) then begin
-      url:=RegExSubstitute('(https?://|www\.)geo\.komurka\.cz\/check\.php([^"'']+)',GC.LongDescription,'$0#'); // parsnout url z listingu, GC.LongDescription (konci '#')
+    {
+    KOMURKA
+    url: http://geo.komurka.cz/check.php?cache=GC2JCEQ
+    captcha: ano
+    }
+    else if RegexFind('https?://(www\.)?geo\.komurka\.cz\/check\.php([^"'']+)',GC.LongDescription) then begin
+      url:=RegExSubstitute('https?://(www\.)?geo\.komurka\.cz\/check\.php([^"'']+)',GC.LongDescription,'$0#'); // parsnout url z listingu, GC.LongDescription (konci '#')
       service:='komurka';
-      {
-      url: http://geo.komurka.cz/check.php?cache=GC2JCEQ
-      captcha: ano
-      }
     end
-    else if RegexFind('(https?://|www\.)gccounter\.(de|com)\/gcchecker\.php([^"'']+)',GC.LongDescription) then begin
-      url:=RegExSubstitute('(https?://|www\.)gccounter\.(de|com)\/gcchecker\.php([^"'']+)',GC.LongDescription,'$0#'); // parsnout url z listingu, GC.LongDescription (konci '#')
+    {
+    GCCOUNTER
+    url: http://gccounter.com/gcchecker.php?site=gcchecker_check&id=2076
+    captcha: ne
+    }
+    else if RegexFind('https?://(www\.)?gccounter\.(de|com)\/gcchecker\.php([^"'']+)',GC.LongDescription) then begin
+      url:=RegExSubstitute('https?://(www\.)?gccounter\.(de|com)\/gcchecker\.php([^"'']+)',GC.LongDescription,'$0#'); // parsnout url z listingu, GC.LongDescription (konci '#')
       service:='gccounter';
-      {
-      url: http://gccounter.com/gcchecker.php?site=gcchecker_check&id=2076
-      captcha: ne
-      }
     end
-    else if RegexFind('(https?://|www\.)certitudes\.org\/certitude\?wp\=([^"'']+)',GC.LongDescription) then begin
-      url:=RegExSubstitute('(https?://|www\.)certitudes\.org\/certitude\?wp\=([^"'']+)',GC.LongDescription,'$0#'); // parsnout url z listingu, GC.LongDescription (konci '#')
+    {
+    CERTITUDES
+    url: http://www.certitudes.org/certitude?wp=GC2QFYT
+    captcha: ne
+    }
+    else if RegexFind('https?://(www\.)?certitudes\.org\/certitude\?wp\=([^"'']+)',GC.LongDescription) then begin
+      url:=RegExSubstitute('https?://(www\.)?certitudes\.org\/certitude\?wp\=([^"'']+)',GC.LongDescription,'$0#'); // parsnout url z listingu, GC.LongDescription (konci '#')
       service:='certitudes';
-      {
-      url: http://www.certitudes.org/certitude?wp=GC2QFYT
-      captcha: ne
-      }
     end
+    {
+    FINAR - secret behavior
+    url: http://gc.elanot.cz/index.php/data-final.html?fabrik_list_filter_all_1_com_fabrik_1=N+49%C2%B0+06.864+E+017%C2%B0+46.694&limit1=10&limitstart1=0&option=com_fabrik&orderdir=&orderby=&view=list&listid=1&listref=1_com_fabrik_1&Itemid=112&fabrik_referrer=%2Findex.php%2Fdata-final.html%3Fresetfilters%3D0&735d56ae937921f2bb7c794d8ebfee41=1&format=html&packageId=0&task=list.filter&fabrik_listplugin_name=&fabrik_listplugin_renderOrder=&fabrik_listplugin_options=&incfilters=1
+    captcha: ne
+    }
     else if FileExists(GEOGET_SCRIPTDIR+'\Checker\finar.txt') then begin
-      {secret behavior}
       url:='http://gc.elanot.cz/index.php/data-final.html';
       service:='finar';
-      {
-      url: http://gc.elanot.cz/index.php/data-final.html?fabrik_list_filter_all_1_com_fabrik_1=N+49%C2%B0+06.864+E+017%C2%B0+46.694&limit1=10&limitstart1=0&option=com_fabrik&orderdir=&orderby=&view=list&listid=1&listref=1_com_fabrik_1&Itemid=112&fabrik_referrer=%2Findex.php%2Fdata-final.html%3Fresetfilters%3D0&735d56ae937921f2bb7c794d8ebfee41=1&format=html&packageId=0&task=list.filter&fabrik_listplugin_name=&fabrik_listplugin_renderOrder=&fabrik_listplugin_options=&incfilters=1
-      captcha: ne
-      }
     end
+    {standard behavior}
     else begin
-      {standard behavior}
       ShowMessage(_('Error: No coordinate checker URL found!'));
       GeoAbort;
     end;
 
-    //ShowMessage('"'+GEOGET_SCRIPTDIR+'\Checker\AutoIt3.exe" "'+GEOGET_SCRIPTDIR+'\Checker\Checker.au3" '+service+' '+CorrectCoords(coord)+' '+TrimUrl(url));
+    if debug<>'0' then ShowMessage('"'+GEOGET_SCRIPTDIR+'\Checker\AutoIt3.exe" "'+GEOGET_SCRIPTDIR+'\Checker\Checker.au3" '+service+' '+CorrectCoords(coord)+' '+TrimUrl(url));
     RunExecNoWait('"'+GEOGET_SCRIPTDIR+'\Checker\AutoIt3.exe" "'+GEOGET_SCRIPTDIR+'\Checker\Checker.au3" '+service+' '+CorrectCoords(coord)+' '+TrimUrl(url));
-  end;
+  end
+  {wrong coordinates, maybe they are zero}
+  else ShowMessage(_('Wrong coordinates. Maybe they are zero'));
 end;
